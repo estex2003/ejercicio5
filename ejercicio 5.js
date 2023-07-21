cb21a4c71dd863f6bbf4f0fb85cc12bdde688828
Esteban <estebanvelascoriano@gmail.com>
2023-07-21 09:53:51 -0500

    se hizo lo que mas se pudo

---
+++ ejercicio 5.js
@@ -0,0 +1,22 @@
+// altura en cm entero
+let altura_cm = 160;
+
+console.log(altura_cm+"cm")
+//altura en m 
+let altura_m = 1.60;
+console.log(altura_cm.toFixed(2)+"m")
+
+//peso
+let peso_kg = Number(63.3)
+console.log(peso_kg.toFixed())
+
+
+//maximos y minimos 
+
+let min_precision = Number.EPSILON
+let minimo_valor_de_js = Number.MIN_VALUE
+let max_valor_js = Number.MAX_VALUE
+
+console.log(min_precision)
+console.log(minimo_valor_de_js)
+console.log(max_valor_js)
